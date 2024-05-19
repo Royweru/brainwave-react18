31ed6f0bf42752ddbc88f13058e401be85282f18
Royweru <royezi17@gmail.com>
2024-05-19 06:49:16 +0300

    modified:   .eslintrc.cjs 	modified:   src/App.jsx 	new file:   src/components/hero.jsx 	new file:   src/components/section.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import ButtonGradient from "./assets/svg/ButtonGradient"
 import { Header } from "./components/header";
+import { Hero } from "./components/hero";
 function App() {
   return (
     <>
       
       <div className=" pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
-      
+        <Hero />
       </div>
       <ButtonGradient />
     </>

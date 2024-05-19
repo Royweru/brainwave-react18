70c88f1fae262ba4ec23f2c9d47e600659475918
Royweru <royezi17@gmail.com>
2024-05-19 07:28:14 +0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/hero.jsx 	modified:   src/components/section.jsx

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,7 +1,13 @@
+/* eslint-disable no-unused-vars */
 import { Section } from "./section";
 import { curve, heroBackground, robot } from "../assets";
+import {BackgroundCircles, BottomLine,Gradient,Rings} from "../components/design/Hero"
+import {HeroIcons, heroIcons} from "../constants"
+import { ScrollParallax } from "react-just-parallax";
+import { useRef } from "react";
 
 export const Hero = () => {
+  const parallaxRef = useRef(null) 
   return (
     <Section
       className=" pt-[12rem] -mt-[5.25rem]"
@@ -10,7 +16,7 @@ export const Hero = () => {
       customPaddings
       id={"hero"}
     >
-      <div className=" container relative">
+      <div className=" container relative" ref={parallaxRef} >
         <div
           className=" relative z-1 max-w-[62rem] mx-auto
              text-center mb-[4rem] md:mb-20 lg:mb-[6rem]
@@ -54,14 +60,27 @@ export const Hero = () => {
                 src={robot} 
                 alt=" hero" 
                 className=" w-full scale-[1.7] translate-y-[8%] md:scale-[1]
-                 md:-translate-y-[10%] lg:-translate-y-[23%]
+                 md:-t
                 "
                  width={1024}
                  height={490}
 
                 />
+                <ScrollParallax isAbsolutelyPositioned>
+                    <ul className=" hidden absolute -left-[5.5rem] bottom-[7.5rem]
+                     px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl
+                     xl:flex
+                    ">
+                       {heroIcons.map((icon,index)=>(
+                        <li key={index} className=" p-5">
+                             <img src={icon} width={24} height={25} alt="Icon" />
+                        </li>
+                       ))}
+                    </ul>
+                </ScrollParallax>
               </div>
             </div>
+            <Gradient />
           </div>
            <div className=" absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2
              md:-top-[46%] md:w-[138%] lg:-top-[104%]
@@ -73,6 +92,7 @@ export const Hero = () => {
               height={1800}
              alt="hero" />
            </div>
+           <BackgroundCircles />
         </div>
       </div>
     </Section>
--- src/components/section.jsx
+++ src/components/section.jsx
@@ -11,12 +11,13 @@ export const Section = ({
 }) => {
   return (
     <div
-      className={` relative ${
-        customPaddings ||
-        `py-10 lg:py-16 xl:py-20
-    ${crosses ? " lg:py-32 xl:py-40" : ""} ${className || ""}
-    `
-      }`}
+    id={id}
+      className={`relative
+       ${customPaddings||
+        ` py-10 lg:py-16 xl:py-20 ${crosses?' lg:py-32 xl:py-40':''}`
+       }
+       ${className ||""}
+      `}
     >
       {children}
 
